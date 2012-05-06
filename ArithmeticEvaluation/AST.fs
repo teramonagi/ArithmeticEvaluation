d5da5b04474cd5b389613add41d05f4ec7462511
teramonagi <teramonagi@gmail.com>
2012-05-06 16:46:25 +0900

    commit

---
+++ ArithmeticEvaluation/AST.fs
@@ -0,0 +1,9 @@
+﻿module AST
+
+//四則演算のための抽象構文木
+type Expression = 
+    | Int    of int
+    | Plus   of Expression * Expression 
+    | Minus  of Expression * Expression 
+    | Times  of Expression * Expression 
+    | Divide of Expression * Expression 
---
+++ ArithmeticEvaluation/Program.fs
@@ -0,0 +1,23 @@
+﻿//各種モジュールの読み込み
+open AST
+open Lexer
+open Parser 
+//文字列→抽象構文木へ
+let parse txt =
+  txt
+  |> Lexing.LexBuffer<_>.FromString
+  |> Parser.Expr Lexer.token
+//評価器・数値に倒して実際の評価を実行
+let rec eval = function
+  | Int i        -> i
+  | Plus (a,b)   -> eval a + eval b
+  | Minus (a,b)  -> eval a - eval b
+  | Times (a,b)  -> eval a * eval b
+  | Divide (a,b) -> eval a / eval b
+ 
+//試しの計算
+do
+  "(4 * (2 + 3))"
+  |> parse 
+  |> eval
+  |> printfn "%d"
